319e89e5a3355168113d1e5e8627aa51c070cea2
mohammedmustakeem <166290261+mohammedmustakeem@users.noreply.github.com>
2024-09-08 05:19:51 -0700

    Add files via upload

---
+++ chatbot (1).py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+# Function to load the Gemini Pro model and get a response
+model = genai.GenerativeModel("gemini-pro")
+chat = model.start_chat(history=[])
+
+def get_gemini_response(question):
+    response = chat.send_message(question, stream=True)
+    return response
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini LLM Application")
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+input_text = st.text_input("Input:", key="input")
+submit = st.button("Ask the question")
+
+if submit and input_text:
+    response = get_gemini_response(input_text)
+    st.session_state['chat_history'].append(("You", input_text))
+    st.subheader("The Response is")
+
+    # Ensure the response is handled correctly
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot", chunk.text))
+
+st.subheader("The Chat History is")
+
+for role, text in st.session_state['chat_history']:
+    st.write(f"{role}: {text}")
